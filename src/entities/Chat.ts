620a18ccfa7bbef190caf6bcebebc4f92c8cbc4c
limgeun <shing100@naver.com>
2019-01-15 21:39:57 +0900

    Chat, Message 엔터티 추가작업

---
+++ src/entities/Chat.ts
@@ -0,0 +1,24 @@
+import {
+    BaseEntity,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    OneToMany,
+} from "typeorm";
+
+import Message from "./Message";
+
+@Entity()
+class Chat extends BaseEntity {
+    @PrimaryGeneratedColumn() id: number;
+
+    @OneToMany(type => Message, message => message.chat)
+    messages: Message[];
+
+    @CreateDateColumn() createdAt: string;
+
+    @UpdateDateColumn() updatedAt: string;
+}
+
+export default Chat;
