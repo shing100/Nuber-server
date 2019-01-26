e404d44964ff37fd903551662b77a1e4a1573305
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-01-26 23:01:02 +0900

    Email 회원가입 기능 추가

--- src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -5,7 +5,7 @@ import User from "src/entities/User";
 const resolvers: Resolvers = {
     Mutation: {
         EmailSignIn: async(_, args:EmailSignInMutationArgs): Promise<EmailSignInResponse> => {
-            const { email } = args;
+            const { email, password } = args;
             try {
                 const user = await User.findOne({ email });
                 if(!user){
@@ -15,6 +15,21 @@ const resolvers: Resolvers = {
                         token: null
                     } 
                 }
+                const checkPassword = await user.comparePassword(password);
+                if(checkPassword){
+                    return {
+                        ok: true,
+                        error: null,
+                        token: "Comming soon"
+                    }
+                }else{
+                    return {
+                        ok: false,
+                        error: "Wrong password",
+                        token: null
+                    }
+                }
+
             }catch(error) {
                 return {
                     ok: false,
