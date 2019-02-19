7081a6d7b737409c4eb514e6e0f3868263f9f2fc
limgeun <shing100@naver.com>
2019-02-19 22:41:43 +0900

    StartPhoneVerification 작업

--- src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
+++ src/api/User/StartPhoneVerification/StartPhoneVerification.resolvers.ts
@@ -1,8 +1,28 @@
-import { Resolvers } from "src/types/resolvers";
+import Verification from "../../../entities/Verification";
+import { StartPhoneVerificationMutationArgs, StartPhoneVerificationResponse } from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
 
-const resolvers: Resolvers {
+const resolvers: Resolvers = {
     Mutation: {
-        StartPhoneVerification: async () => ""
+        StartPhoneVerification: async (
+            _,
+            args: StartPhoneVerificationMutationArgs
+        ): Promise<StartPhoneVerificationResponse> => {
+            const { phoneNumber } = args;
+            try {
+                const existingVerification = await Verification.findOne({
+                    payload: phoneNumber
+                });
+                if (existingVerification) {
+                    existingVerification.remove();
+                }
+            } catch (error) {
+                return {
+                    ok: false,
+                    error: error.message
+                };
+            }
+        }
     }
 };
 
