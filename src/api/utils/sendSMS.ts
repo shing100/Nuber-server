72b7cf30a6109ffa1700541f654cd1c47e689c3f
limgeun <shing100@naver.com>
2019-02-21 14:35:29 +0900

    인증번호 문제 메세지문구 변경

--- src/api/utils/sendSMS.ts
+++ src/api/utils/sendSMS.ts
@@ -12,4 +12,4 @@ export const sendSMS = (to: string, body: string): Promise<any> => {
 }
 
 // 인증 텍스트 메시지 전송
-export const sendVerificationSMS = (to: string, key: string) => sendSMS(to, `인증번호는 ${key} 입니다.`);
+export const sendVerificationSMS = (to: string, key: string) => sendSMS(to, `본인확인 인증번호 [${key}]를 입력해주세요`);
