c71daaab070ee4575f25b32e48e4548423767c26
DESKTOP-CN9OS9T\shing <shing100@naver.com>
2019-04-02 21:38:14 +0900

    EditPlace Resolver

---
+++ src/api/Place/EditPlace/EditPlace.resolvers.ts
@@ -0,0 +1,45 @@
+import { Resolvers } from "../../../types/resolvers";
+import privateResolver from "../../../utils/privateResolver";
+import User from "../../../entities/User";
+import Place from "../../../entities/Place";
+import cleanNullArgs from "../../../utils/cleanNullArgs";
+import { EditPlaceResponse, EditPlaceMutationArgs } from "../../../types/graph";
+
+
+const resolvers: Resolvers = {
+    Mutation: {
+        EditPlace: privateResolver(async(_, args: EditPlaceMutationArgs, { req }): Promise<EditPlaceResponse> => {
+            const user:User = req.user;
+            try {
+                const place = await Place.findOne({id: args.placeId});
+                if(place){
+                    if(place.userId === user.id) {
+                        const notNull = cleanNullArgs(args);
+                        await Place.update({id: args.placeId} {...notNull});
+                        return {
+                            ok: true,
+                            error: null
+                        }
+                    }else {
+                        return {
+                            ok: false,
+                            error: "인증필요"
+                        }
+                    }
+                }else {
+                    return {
+                        ok: false,
+                        error: "place not found"
+                    }
+                }
+            } catch (error) {
+                return {
+                    ok: false,
+                    error: error.message
+                }
+            }
+        })
+    }
+}
+
+export default resolvers;
